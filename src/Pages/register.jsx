96d5db3be6a0ee17df0157bf8412f1d9d6bf26bf
zaazxz <mirzaqamaruzzaman18@gmail.com>
2024-06-26 22:52:50 +0700

    feat [react] : react-router-dom

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import FormRegister from '../components/Fragments/FormRegister'
 import AuthLayout from '../components/Layouts/AuthLayout'
+import { Link } from 'react-router-dom'
 
 const RegisterPage = () => {
   return (
     <AuthLayout title="Register">
         <FormRegister />
+        <p className='text-slate-500 mt-3'>
+          Already have an account? <Link to="/login" className='text-blue-600 underline'>Login</Link>
+        </p>
     </AuthLayout>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LoginPage from './Pages/login.jsx'
+import RegisterPage from './Pages/register.jsx'
+import ErrorPage from './Pages/404.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello World!</div>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
+  {
+    path: '/register',
+    element: <RegisterPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
