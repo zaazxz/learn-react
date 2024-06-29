caefaedb369904e23d648066d18d00c8c3610789
zaazxz <mirzaqamaruzzaman18@gmail.com>
2024-06-30 04:24:53 +0700

    feat [react] : Event Handler

--- src/components/Elements/Button/Index.jsx
+++ src/components/Elements/Button/Index.jsx
@@ -1,6 +1,10 @@
 const Button = (props) => {
+
+  // Deafult 
+  const { classname = "bg-black", type = "button", onclick = () => {} } = props;
+
   return (
-    <button className={`h-10 px-6 font-semibold rounded-md ${props.classname} text-white`}>
+    <button className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`} onClick={() => onclick()} type={type}>
       {props.children}
     </button>
   );
