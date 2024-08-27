9c6c6b61f16cd664a603871350a26071c23ddf5f
zaazxz <mirzaqamaruzzaman18@gmail.com>
2024-08-27 07:50:10 +0700

    feat [react] : React Router

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import AuthLayouts from '../components/Layouts/AuthLayouts'
 import FormRegister from '../components/fragments/FormRegister'
+import { Link } from 'react-router-dom'
 
 const RegisterPage = () => {
   return (
     <AuthLayouts title="Register">
       <FormRegister />
+      <p className="mt-4 text-sm text-center">
+        Already have an account? <Link to="/login" className="font-bold text-blue-600">Sign in</Link>
+      </p>
     </AuthLayouts>
   )
 }
--- src/components/Elements/Input/Input.jsx
+++ src/components/Elements/Input/Input.jsx
@@ -10,6 +10,7 @@ const Input = (props) => {
             className="text-sm border rounded w-full py-2 px-2 text-slate-700 placeholder:opacity-50"
             placeholder={placeholder}
             name={name}
+            id={name}
         />
     )
 }
