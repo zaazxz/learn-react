24861c2f6cb2c8dffba9cd58028264131a56917d
zaazxz <mirzaqamaruzzaman18@gmail.com>
2024-08-27 10:17:51 +0700

    feat [react] : Conditional Rendering

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import AuthLayouts from '../components/Layouts/AuthLayouts'
 import FormRegister from '../components/fragments/FormRegister'
-import { Link } from 'react-router-dom'
 
 const RegisterPage = () => {
   return (
-    <AuthLayouts title="Register">
+    <AuthLayouts title="Register" type="register">
       <FormRegister />
-      <p className="mt-4 text-sm text-center">
-        Already have an account? <Link to="/login" className="font-bold text-blue-600">Sign in</Link>
-      </p>
     </AuthLayouts>
   )
 }
