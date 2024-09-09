a1d9d33ea0c3707e0c7c9a5f3d23f4f9ebdcc341
zaazxz <mirzaqamaruzzaman18@gmail.com>
2024-09-09 12:59:18 +0700

    feat [react] : Redux Orientation

---
+++ redux.js
@@ -0,0 +1,32 @@
+import { legacy_createStore } from "redux";
+
+// Reducer
+const cartReducer = (
+    state = {
+        cart: [{ id: 1, qty: 10 }],
+    },
+    action
+) => {
+    switch (action.type) {
+        case "ADD_TO_CART":
+            return {
+                ...state,
+                cart: [...state.cart, action.payload],
+            };
+        default:
+            return state;
+    }
+};
+
+// Store
+const store = legacy_createStore(cartReducer)
+console.log("on create store : ", store.getState());
+
+// Subscribe
+store.subscribe(() => {
+    console.log("on subscribe : ", store.getState());
+});
+
+// Dispatch
+const action1 = { type: "ADD_TO_CART", payload: { id: 2, qty: 20 } };
+store.dispatch(action1);
